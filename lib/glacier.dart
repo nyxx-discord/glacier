c7b123c5acd56569506e6917d3a3f9976924896a
HarryET <29015545+HarryET@users.noreply.github.com>
2021-07-12 17:18:38 +0100

    Initial Commit
    Has init support, needs directory util fixes

---
+++ lib/glacier.dart
@@ -0,0 +1,10 @@
+library glacier;
+
+import "dart:io";
+
+import "package:yaml/yaml.dart";
+
+part "src/ConfigUtils.dart";
+part "src/DirectoryUtils.dart";
+part "src/internal/GlacierConfig.dart";
+part "src/internal/_YamlWriter.dart";
---
+++ lib/src/DirectoryUtils.dart
@@ -0,0 +1,9 @@
+part of glacier;
+
+// TODO fix?
+class DirectoryUtils {
+  static String get osSlash => Platform.isWindows ? "\\" : "/";
+
+  // TODO remove .replaceAll and use osSlash
+  static String get currentDirName => Directory.current.path.replaceAll("\\", "/").split("/").last;
+}

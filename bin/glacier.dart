74c2ca1354e83f4ca3f63c4d5db11de38df4a92f
HarryET <29015545+HarryET@users.noreply.github.com>
2021-07-27 15:06:14 +0100

    Refactor to use Host & UI

--- bin/glacier.dart
+++
@@ -1,9 +0,0 @@
-import "package:args/command_runner.dart";
-
-import "commands/init_command.dart";
-
-Future<void> main(List<String> args) async {
-  final commandRunner = CommandRunner<void>("glacier", "Dart markdown docs gen")..addCommand(InitCommand());
-
-  return commandRunner.run(args);
-}
---
+++ glacier_host/bin/glacier.dart
@@ -0,0 +1,7 @@
+Future<void> main(List<String> args) async {
+  // TODO Check if config & .md files exist
+
+  // TODO cache files/directories
+
+  // TODO create & start api
+}

2f800e0ea59722180ce053d82626908f368d5a3d
Arsenii Liunsha <me@arslee.dev>
2021-08-29 14:44:53 +0900

    Add serve command

--- cli/bin/glacier.dart
+++ cli/bin/glacier.dart
@@ -4,7 +4,8 @@ import "package:glacier/glacier.dart";
 Future<void> main(List<String> args) async {
   final commandRunner = CommandRunner<void>("glacier", "Dart markdown docs gen")
     ..addCommand(InitCommand())
-    ..addCommand(GenerateCommand());
+    ..addCommand(GenerateCommand())
+    ..addCommand(ServeCommand());
 
   return commandRunner.run(args);
 }
--- cli/lib/glacier.dart
+++ cli/lib/glacier.dart
@@ -9,10 +9,13 @@ import "package:http/http.dart" as http;
 import "package:markdown/markdown.dart";
 import "package:mustache_template/mustache.dart";
 import "package:path/path.dart" as path;
+import "package:shelf_static/shelf_static.dart" as shelf_static;
+import "package:shelf/shelf_io.dart" as shelf_io;
 import "package:yaml/yaml.dart";
 
 part "commands/InitCommand.dart";
 part "commands/GenerateCommand.dart";
+part "commands/ServeCommand.dart";
 
 part "internal/_YamlWriter.dart";
 part "internal/GlacierConfig.dart";
